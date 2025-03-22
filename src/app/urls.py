26f418177ae73b69b14685dcc19ff2759bf02afa
AZEEZ RIDWAN <78234083+Zizou897@users.noreply.github.com>
2025-03-22 02:17:46 +0100

    fix: add all page

--- src/app/urls.py
+++ src/app/urls.py
@@ -2,9 +2,13 @@ from django.urls import path
 from .views import (
     home,
     contact,
+    cas,
+    about,
 )
 
 urlpatterns = [
     path('', home, name='welcome'),
     path('contact', contact, name='contact'),
+    path('cas', cas, name='cas'),
+    path('a-props', about, name='about'),
 ]
--- src/app/views.py
+++ src/app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 
 from .functions import (
-    get_some_cas
+    get_some_cas,
+    get_all_cas
 )
 # Create your views here.
 
@@ -19,10 +20,32 @@ def home(request):
 
 def contact(request):
     
-    get_some_cass = get_some_cas({'publish':True})
+    get_all_cass = get_all_cas({'publish':True})
     
     template_name = "app/layouts/contact.html"
     context = {
-        'get_some_cas': get_some_cass
+        'get_all_cas': get_all_cass
+    }
+    return render(request, template_name, context)
+
+
+def cas(request):
+    
+    get_all_cass = get_all_cas({'publish':True})
+    
+    template_name = "app/layouts/cas.html"
+    context = {
+        'get_all_cas': get_all_cass
+    }
+    return render(request, template_name, context)
+
+
+def about(request):
+    
+    get_all_cass = get_all_cas({'publish':True})
+    
+    template_name = "app/layouts/about.html"
+    context = {
+        'get_all_cas': get_all_cass
     }
     return render(request, template_name, context)
